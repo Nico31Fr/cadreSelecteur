2338d646d47179a4a16ef57125a537e07f1a8799
Nicolas Cot <cot.nicolas@gmail.com>
2025-12-29 07:34:41 +0100

    mise à jour pour compatibilité Windows.

--- CadreSelecteur/__main__.py
+++ CadreSelecteur/__main__.py
@@ -2,15 +2,26 @@
 """ Module de selection de cadre pour PiBooth
     |→ point d'entrée de l'appli autonome """
 
-from multiprocessing import Process
+from multiprocessing import Process, freeze_support
 from time import sleep
 
 from CadreSelecteur.cadreselecteur import CadreSelecteur, check_mandatory_path
 from CadreSelecteur.splash import splash
 
+
+def run_splash():
+    splash()
+
+
+def run_app():
+    CadreSelecteur()
+
+
 if __name__ == "__main__":
-    splash_process = Process(target=lambda: splash())
-    app_process = Process(target=lambda: CadreSelecteur())
+    freeze_support()  # indispensable pour Windows + PyInstaller
+
+    splash_process = Process(target=run_splash)
+    app_process = Process(target=run_app)
     splash_process.start()
 
     # Vérifie les path
@@ -19,6 +30,6 @@ if __name__ == "__main__":
     # start IHM
     app_process.start()
 
-    # fermer le splash
+    # fermer le splash sreen
     sleep(3)
     splash_process.terminate()
